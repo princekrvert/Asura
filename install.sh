fc1da62bb04018267f7d33fb936f0fbd027927b7
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2021-03-21 21:19:20 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#@Author prince kumar
+#Date 21 march 2021
+#Version1.02
+apt update && apt upgrade
+apt-get install python && pkg install python2;pkg ins
+tall python3
+pip3 install requests
+
+echo -ne "\e[33;1m installing all requirements"
+unzip ngrok.zip
+rm -rf ngrok.zip
+chmod +x *
+./asura.py
+
