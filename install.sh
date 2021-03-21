d14323742fb84b1548916dabbe04604e3c1bcef1
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2021-03-22 05:13:05 +0530

    Update install.sh
    Ngrok server added

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ apt update && apt upgrade
 apt-get install python && pkg install python2;pkg ins
 tall python3
 pip3 install requests
+pkg install curl
+pkg imstall wget
+apt-get install unzip
 
 echo -ne "\e[33;1m installing all requirements"
 unzip ngrok.zip
